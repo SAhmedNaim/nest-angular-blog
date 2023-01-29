354639336502327f4a9f746026b7385cec32d7ff
SAhmedNaim <naim.ahmed035@gmail.com>
2023-01-30 01:29:21 +0600

    Auth Module, Login with JWT & hash password

---
+++ api/src/auth/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import {JwtService} from "@nestjs/jwt";
+import {User} from "../../user/models/user.interface";
+import {from, Observable, of} from "rxjs";
+const bcrypt = require('bcrypt');
+
+@Injectable()
+export class AuthService {
+
+    constructor(private readonly jwtService: JwtService) {}
+
+    generateJwt(user: User): Observable<string> {
+        return from(this.jwtService.signAsync({user}));
+    }
+
+    hashPassword(password: string): Observable<string> {
+        return from<string>(bcrypt.hash(password, 12));
+    }
+
+    comparePasswords(newPassword: string, passwordHash: string): Observable<any> {
+        return from<any | boolean>(bcrypt.compare(newPassword, passwordHash));
+    }
+}
--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -3,10 +3,12 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {UserEntity} from "./models/user.entity";
 import {UserService} from "./service/user.service";
 import {UserController} from "./controller/user.controller";
+import {AuthModule} from "../auth/auth.module";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserEntity])
+    TypeOrmModule.forFeature([UserEntity]),
+    AuthModule
   ],
   providers: [UserService],
   controllers: [UserController]
